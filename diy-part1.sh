a11f9071f75d3a81178875922d80e20da445b94a
Townbox <91249891+Townbox@users.noreply.github.com>
2023-05-20 23:52:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,10 @@
 sed -i 's/^#\(.*kenzo\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*small\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*lienol\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*packages\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*routing\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -23,3 +27,8 @@ sed -i 's/^#\(.*lienol\)/\1/' feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+
+echo 'src-git packages https://github.com/immortalwrt/packages' >>feeds.conf.defaul
+echo 'src-git luci https://github.com/immortalwrt/luci' >>feeds.conf.defaul
+echo 'src-git routing https://github.com/openwrt/routing' >>feeds.conf.defaul
+echo 'src-git telephony https://github.com/openwrt/telephony' >>feeds.conf.defaul
